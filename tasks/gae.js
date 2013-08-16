e73312a59f74e3dc943569a67b6a2fd550fa0233
Maciej Zasada <maciejzsd@gmail.com>
2013-08-16 18:29:49 +0200

    run and deploy tasks (WIP)

--- tasks/gae.js
+++ tasks/gae.js
@@ -8,43 +8,98 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('gae', 'Google App Engine deployment plugin for Grunt.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+    var sys = require('sys'),
+        exec = require('child_process').exec,
+        format = require('util').format;
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+    grunt.registerMultiTask('gae', 'Google App Engine deployment plugin for Grunt.', function () {
+
+        // Merge task-specific and/or target-specific options with these defaults.
+        var options = this.options({
+                application: '',
+                yaml: 'app.yaml',
+                port: 8080,
+                admin_port: 8000
+            }),
+            childProcess,
+            action = this.data.action,
+            done = this.async();
+
+        // Check if action was specified.
+        if (!action) {
+            return grunt.log.error('No action specified.');
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
 
-      // Handle options.
-      src += options.punctuation;
+        // Check if the specified action is correct.
+        if (['run', 'deploy'].indexOf(action) === -1) {
+            return grunt.log.error(format('%s is not a valid gae action.', action));
+        }
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+        // Handle the action specified
+        switch(action) {
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+            case 'run':
+                grunt.log.writeln('RUNNING');
+                childProcess = exec('pwd', {}, function () {
+                    grunt.log.writeln('callback');
+                });
+                break;
+
+            case 'deploy':
+                grunt.log.writeln('DEPLOYING');
+                childProcess = exec('pwd', {}, function () {
+                    grunt.log.writeln('callback');
+                });
+                break;
+
+        }
+
+        childProcess.stdout.on('data', function (d) {
+            grunt.log.write(d);
+        });
+        childProcess.stderr.on('data', function (d) {
+            grunt.log.error(d);
+        });
+
+        childProcess.on('exit', function (code) {
+            if (code !== 0) {
+                grunt.log.error(format('Exited with code: %d.', code));
+                return done(false);
+            }
+            grunt.verbose.ok(format('Exited with code: %d.', code));
+            done();
+        });
+
+        /*
+         // Iterate over all specified file groups.
+         this.files.forEach(function(f) {
+         // Concat specified files.
+         var src = f.src.filter(function(filepath) {
+         // Warn on and remove invalid source files (if nonull was set).
+         if (!grunt.file.exists(filepath)) {
+         grunt.log.warn('Source file "' + filepath + '" not found.');
+         return false;
+         } else {
+         return true;
+         }
+         }).map(function(filepath) {
+         // Read file source.
+         return grunt.file.read(filepath);
+         }).join(grunt.util.normalizelf(options.separator));
+
+         // Handle options.
+         src += options.punctuation;
+
+         // Write the destination file.
+         grunt.file.write(f.dest, src);
+
+         // Print a success message.
+         grunt.log.writeln('File "' + f.dest + '" created.');
+         */
     });
-  });
-
 };
--- test/gae_test.js
+++ test/gae_test.js
@@ -3,46 +3,58 @@
 var grunt = require('grunt');
 
 /*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
+ ======== A Handy Little Nodeunit Reference ========
+ https://github.com/caolan/nodeunit
 
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
+ Test methods:
+ test.expect(numAssertions)
+ test.done()
+ Test assertions:
+ test.ok(value, [message])
+ test.equal(actual, expected, [message])
+ test.notEqual(actual, expected, [message])
+ test.deepEqual(actual, expected, [message])
+ test.notDeepEqual(actual, expected, [message])
+ test.strictEqual(actual, expected, [message])
+ test.notStrictEqual(actual, expected, [message])
+ test.throws(block, [error], [message])
+ test.doesNotThrow(block, [error], [message])
+ test.ifError(value)
+ */
 
 exports.gae = {
-  setUp: function(done) {
-    // setup here if necessary
-    done();
-  },
-  default_options: function(test) {
-    test.expect(1);
+    setUp: function (done) {
+        // setup here if necessary
+        done();
+    },
+    run_default: function (test) {
+        test.expect(1);
 
-    var actual = grunt.file.read('tmp/default_options');
-    var expected = grunt.file.read('test/expected/default_options');
-    test.equal(actual, expected, 'should describe what the default behavior is.');
+//    var actual = grunt.file.read('tmp/default_options');
+//    var expected = grunt.file.read('test/expected/default_options');
+//    test.equal(actual, expected, 'should describe what the default behavior is.');
+        test.equal(1, 1, 'TODO: implement the test');
 
-    test.done();
-  },
-  custom_options: function(test) {
-    test.expect(1);
+        test.done();
+    },
+    run_custom: function (test) {
+        test.expect(1);
 
-    var actual = grunt.file.read('tmp/custom_options');
-    var expected = grunt.file.read('test/expected/custom_options');
-    test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
+//    var actual = grunt.file.read('tmp/custom_options');
+//    var expected = grunt.file.read('test/expected/custom_options');
+//    test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
+        test.equal(1, 1, 'TODO: implement the test');
 
-    test.done();
-  },
+        test.done();
+    },
+    deploy_default: function (test) {
+        test.expect(1);
+        test.equal(1, 1, 'TODO: implement the test');
+        test.done();
+    },
+    deploy_custom: function (test) {
+        test.expect(1);
+        test.equal(1, 1, 'TODO: implement the test');
+        test.done();
+    }
 };
